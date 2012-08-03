241ef5d769c656ba46839463b84390d10cef90e7
nathan king <nathan@betapoint.tv>
2012-08-03 18:06:57 -0400

    index is now welcome, new index is going to be for login only

--- setup/welcome.php
+++ setup/welcome.php
@@ -66,8 +66,21 @@
       </div>
     </div>
 
-    
-        <div class="span12">
+    <div class="container-fluid">
+      <div class="row-fluid">
+        <div class="span3">
+          <div class="well sidebar-nav">
+            <ul class="nav nav-list">
+              <li class="nav-header">Available functions</li>
+              <!-- <li class="active"><a href="#">Twitter Proxy</a></li> -->
+              <li><a href="#">Twitter Proxy</a></li>
+              <li><a href="#">Location Search</a></li>
+              <li><a href="#">Data Store</a></li>
+              <li><a href="#">User Upload</a></li>
+            </ul>
+          </div><!--/.well -->
+        </div><!--/span-->
+        <div class="span9">
           <div class="hero-unit">
             <h1>Hello, creative person!</h1>
             <p>adsBackend is meant to be a quick setup on heroku (<a href="http://www.heroku.com/" target="_blank">http://www.heroku.com/</a>) to give your ads some much needed backend support for any campaign</p>
@@ -76,6 +89,30 @@
             	<p><a class="btn btn-primary btn-large">Get Started &raquo;</a></p>
             </form>
           </div>
+            <ul class="breadcrumb">
+			    <li class="active">
+			    	Currently Active Functions
+			    </li>
+		    </ul>
+          <div class="row-fluid">
+            <div class="span4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->
+            <div class="span4">
+              <h2>Heading</h2>
+              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+              <p><a class="btn" href="#">View details &raquo;</a></p>
+            </div><!--/span-->
+        </div><!--/span-->
+      </div><!--/row-->
+
       <hr>
 
       <footer>
