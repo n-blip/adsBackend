9b099772b65f7dc9384163c7e6daee364c645ef4
nathan king <nathan@betapoint.tv>
2012-08-03 15:35:16 -0400

    checking env vars details

--- includes/heroku.php
+++ includes/heroku.php
@@ -1,4 +1,42 @@
 <?php
+
+if(	isset($_ENV['CLEARDB_DATABASE_URL']) && 
+	($_ENV['CLEARDB_DATABASE_URL']!= '') && 
+	(strstr($_ENV['CLEARDB_DATABASE_URL'], 'mysql://') != FALSE)) {
+	echo('in the if true statement <br />');
+	
+	//parse out the heroku details for cleardb
+	$dbDetails = substr($_ENV['CLEARDB_DATABASE_URL'], 7);
+	
+	echo('dbdetails -----------------------------------<br />');
+	echo('<pre>');
+		print_r($dbDetails);
+	echo('</pre>');
+	
+	$portions = explode($dbDetails, '@');
+	
+	echo('portions -----------------------------------<br />');
+	echo('<pre>');
+		print_r($portions);
+	echo('</pre>');
+	
+	$loginPass = explode($portions[0], ':');
+	
+	echo('loginPass -----------------------------------<br />');
+	echo('<pre>');
+		print_r($loginPass);
+	echo('</pre>');
+	
+	$login = $loginPass[0];
+	$pass = $loginPass[1];
+	
+	$server = $portions[1];
+
+	echo('login = ' .$login .'<br />');
+	echo('pass = ' .$pass .'<br />');
+	echo('server = ' .$server .'<br />');
+	
+
 	define('DB_USER', 'b23e08cd756bd8');
 	define('DB_PASSWORD', '07a735e2');
 	define('DB_HOST', 'us-cdbr-east.cleardb.com');
@@ -8,4 +46,9 @@
 	
 	//$conn = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
 	mysql_select_db(DB_NAME);
+
+}
+else {
+	die('clearDb details not specified, please make sure this addon is installed');
+}
 ?>
